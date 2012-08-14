041d1af0c7253765b317b329dd156f73a850fb06
DavidToca <david.virusfel@gmail.com>
2012-08-14 21:48:43 +0200

    Added Iteration C2,adding a page layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ActiveRecord::Base
+  #atributes
   attr_accessible :description, :image_url, :price, :title
+  #order on db
+  default_scope order: 'title'
+
+  #validation
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {greater_than_or_equal_to: 0.01, message: "The price has to be greater than zero"}
   validates :title, uniqueness: true, length: {minimum: 10}
