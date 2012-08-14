64c4f6aecf629a721cb637f7a786e2cea0990ad6
DavidToca <david.virusfel@gmail.com>
2012-08-15 01:03:24 +0200

    @WIP page 115

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ActiveRecord::Base
   #validation
 
   validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {greater_than_or_equal_to: 0.01, message: "The price has to be greater than zero"}
+  validates :price, :numericality => {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: {minimum: 10}
   validates :image_url, :format => {:with => /.(gif|jpg|png)$/i , :message => 'must be a ur for GIF, JPG or  PNG image'}
 #  validates :image_url, :format => {:with => $r{\.(gif|jpg|png)$}i, message => 'must be a URL for GIF, JPG or PNG image.'}
