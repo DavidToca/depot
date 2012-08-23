88bc09aadc9451d57b7885ff4689fa607ae95fa3
davidtoca <david.virusfel@gmail.com>
2012-08-22 23:50:45 -0500

    @WIP adding ajax support at cart creation

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,14 +42,15 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-
-#    @line_item = @cart.line_items.build(product: product)
     @line_item = @cart.add_product(product.id)
-
+    @current_item = @line_item
     respond_to do |format|
       if @line_item.save
+        format.js { @current_item }
         format.html { redirect_to store_url }
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        format.xml {render :xml => @line_item, :status => :created, :location => @line_item }
+        
+#        format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
